433c931a26a41b6b60a590dfecdcff04c685792f
shypang <smartdragon417@gmail.com>
2020-01-28 00:10:40 +0900

    init test

---
+++ youtube.py
@@ -0,0 +1,15 @@
+from __future__ import unicode_literals
+import youtube_dl
+
+s= input('유튜브 URL 을 입력하세요: ')
+
+ydl_opts = {
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+    }],
+}
+with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    ydl.download([s])
